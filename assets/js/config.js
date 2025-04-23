70fe351cfea0cda259123933aef963ca60a7b07c
Tarun Kushwah <tarunkush500@gmail.com>
2025-04-23 11:31:18 +0530

    removed the slash

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 // API configuration
-const API_URL = 'https://web-production-e59b.up.railway.app/'; // Change this in production
+const API_URL = 'https://web-production-e59b.up.railway.app'; // Change this in production
